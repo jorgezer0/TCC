9de305641d35e984d3cf71b646b79a8d05a941b5
Jorge Veiga de Souza Junior <jorgeveiga0@gmail.com>
2017-09-19 22:46:34 -0400

    initial commit

---
+++ Assets/_Scripts/FocusManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusManager : MonoBehaviour {
+
+	public Transform player;
+
+	public enum focusType{
+		Area,
+		Path
+	};
+	
+	public focusType type = focusType.Area;
+
+	public List<Transform> focusPoints = new List<Transform>();
+	Vector3 tmpFocus;
+	Vector3 focus = Vector3.zero;
+	int count = 0;
+//	public GameObject debug;
+
+
+	// Use this for initialization
+	void Awake () {
+		tmpFocus = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		debug.transform.position = GetFocus ();
+	}
+
+	public Vector3 GetFocus(){
+		if (type == focusType.Area) {
+			for (int i = 0; i < focusPoints.Count; i++) {
+				tmpFocus += focusPoints [i].position;
+				count++;
+			}
+			focus = tmpFocus / count;
+			count = 0;
+			tmpFocus = Vector3.zero;
+			return focus;
+		} else if (type == focusType.Path) {
+			focus = focusPoints [0].transform.position;
+			if ((player.position - focus).magnitude < 2) {
+				focusPoints [0].gameObject.SetActive (false);
+				focusPoints.Remove(focusPoints[0]);
+			}
+			return focus;
+		}
+		return Vector3.zero;
+	}
+}
---
+++ Assets/_Scripts/Teleport.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+//using UnityEngine.VR;
+using Kino;
+
+public class Teleport : MonoBehaviour {
+
+	public Camera cam;
+	public float rotateSpeed = 2;
+	RaycastHit hit;
+
+	public GameObject controller;
+
+	public GameObject tCursor;
+	private Vector3 tCursor_velocity = Vector3.zero;
+	private Vector3 tDestiny = Vector3.zero;
+	float deltaTime;
+	public float distance;
+	Vector3 vel = Vector3.zero;
+	bool canWarp = false;
+	public float warpTime;
+	AnalogGlitch glitch;
+	Vignette vignette;
+	Bloom bloom;
+	public PostProcessingProfile pProces;
+	PostProcessingProfile tempPProces;
+	BloomModel.Settings ppBloom;
+	ChromaticAberrationModel.Settings ppChromatic;
+	VignetteModel.Settings ppVignette;
+
+	public FocusManager focusManager;
+
+	// Use this for initialization
+	void Start () {
+		glitch = cam.GetComponent<AnalogGlitch> ();
+		vignette = cam.GetComponent<Vignette> ();
+		bloom = cam.GetComponent<Bloom> ();
+//		pProces = cam.GetComponent<PostProcessingProfile> ();
+		ppBloom = pProces.bloom.settings;
+		ppChromatic = pProces.chromaticAberration.settings;
+		ppVignette = pProces.vignette.settings;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+		transform.Rotate(0, Input.GetAxis ("Mouse X") * rotateSpeed, 0);
+		cam.transform.Rotate(-Input.GetAxis ("Mouse Y") * rotateSpeed, 0, 0);
+
+
+		if (Physics.Raycast (cam.transform.position, cam.transform.forward, out hit, distance)) {
+//			Debug.DrawLine (cam.transform.position, hit.point, Color.red, 2f);
+			if (hit.transform.gameObject.layer == 8) {
+//				tCursor.transform.position = hit.point;
+				tCursor.transform.position = Vector3.SmoothDamp (tCursor.transform.position, hit.point, ref tCursor_velocity, 0.25f);
+			}
+			if (Input.GetMouseButtonDown (0)) {
+//				transform.position = hit.point;
+				tDestiny = tCursor.transform.position;
+				StartCoroutine ("TeleportTo");
+			}
+		}
+		if (canWarp) {
+			pProces.motionBlur.enabled = true;
+			transform.position = Vector3.SmoothDamp (transform.position, tDestiny, ref vel, warpTime);
+			transform.LookAt (focusManager.GetFocus ());
+			Vector3 normalize = new Vector3 (0, transform.rotation.eulerAngles.y, 0);
+			transform.rotation = Quaternion.Euler (normalize);
+		}
+	}
+
+	IEnumerator TeleportTo(){
+		float step = 0.01f;
+		bool grow = true;
+		int speed = 5;
+		while (step > 0) {
+			if (grow) {
+				step += Time.deltaTime * speed;
+			} else {					 
+				step -= Time.deltaTime * speed*2;
+			}
+			glitch.scanLineJitter = step/2;
+			glitch.colorDrift = step;
+//			bloom.intensity = step*10;
+//			vignette.intensity = step;
+			ppBloom.bloom.intensity = step;
+			ppChromatic.intensity = step;
+			ppVignette.intensity = step;
+			pProces.bloom.settings = ppBloom;
+			pProces.chromaticAberration.settings = ppChromatic;
+			pProces.vignette.settings = ppVignette;
+			if (step >= 1) {
+				grow = false;
+//				transform.position = tCursor.transform.position;
+			}
+			if (step >= 0.5f) {
+				canWarp = true;
+			}
+
+			yield return new WaitForSeconds (1/(deltaTime * 1000f));
+		}
+		pProces.motionBlur.enabled = false;
+		glitch.scanLineJitter = 0;
+		glitch.horizontalShake = 0;
+		glitch.colorDrift = 0;
+//		bloom.intensity = 0;
+//		vignette.intensity = 0;
+		ppBloom.bloom.intensity = 0;
+		ppChromatic.intensity = 0;
+		ppVignette.intensity = 0;
+		canWarp = false;
+		pProces.bloom.settings = ppBloom;
+		pProces.chromaticAberration.settings = ppChromatic;
+		pProces.vignette.settings = ppVignette;
+		yield return null;
+	}
+}
